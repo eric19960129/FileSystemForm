d943289aa9a162fc39910337904cf7b900721d3d
eric19960129 <eric19960129@gmail.com>
2022-05-28 23:43:49 +0800

    prototype

---
+++ FileSystemForm/FileSystemForm/FileReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSystemForm
+{
+    public partial class FileReader : Form
+    {
+        public FileReader(string path)
+        {
+            InitializeComponent();
+
+            pictureBox1.Visible = false;
+            textBox1.Visible = false;
+
+            if ("*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.bmp;*.dib;*.gif;*.tif;*.tiff".Contains(System.IO.Path.GetExtension(path)))
+            {
+                this.Width = Image.FromFile(path).Width + 17;
+                this.Height = Image.FromFile(path).Height + 40;
+
+                pictureBox1.Width = Image.FromFile(path).Width;
+                pictureBox1.Height = Image.FromFile(path).Height;
+                pictureBox1.Image = Image.FromFile(path);
+
+                pictureBox1.Visible = true;
+            }
+            else if ("*.txt".Contains(System.IO.Path.GetExtension(path)))
+            {
+                string readTxt = "";
+                using (StreamReader streamReader = new StreamReader(path))
+                {
+                    while (streamReader.Peek() > 0)
+                    {
+                        readTxt += streamReader.ReadLine() + Environment.NewLine;
+                    }
+                }
+                textBox1.Text = readTxt;
+                textBox1.Visible = true;
+            }
+        }
+    }
+}
---
+++ FileSystemForm/FileSystemForm/Form_File.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;//Image
+using System.IO;//StreamReader、Path
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSystemForm
+{
+    public partial class Form_File : Form
+    {
+        public Form_File()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select File";
+            //File.Filter | 分隔線不能有空格(會導致無法呈現)
+            openFileDialog.Filter = "photo files (*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.bmp;*.dib;*.gif;*.tif;*.tiff)|*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.bmp;*.dib;*.gif;*.tif;*.tiff|txt files (*.txt)|*.txt|office files (*.docx;*.ppt;*.xlsx)|*.docx;*.ppt;*.xlsx|All files (*.*)|*.*";
+            if(openFileDialog.ShowDialog()==System.Windows.Forms.DialogResult.OK && openFileDialog.FileName != null) 
+            {
+                /*
+                 * GetFullPath()    //取得絕對路徑  == openFileDialog.FileName == 絕對路徑(包含檔名)
+                 * GetDirectoryName //取得路徑      == 不包含檔名
+                 * GetExtension()   //取得副檔名 
+                 * GetFileNameWithoutExtension()    //取得檔案名稱，不要副檔名
+                 * GetFileName()    //取得檔案名稱
+                 */
+                label_File.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+                FileReader fileReader = new FileReader(openFileDialog.FileName);
+                fileReader.ShowDialog();
+            }
+        }
+    }
+}
